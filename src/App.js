85eb9ff4fbedee055c9d4069ade2570bdc0bb7b8
jimmy neutron <95525638+dlwnstjrzz@users.noreply.github.com>
2023-09-30 12:14:38 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import socketIO from "socket.io-client";
 import { Home, ChatPage } from "./pages";
 
-const socket = socketIO.connect("http://localhost:4000");
+const socket = socketIO.connect("https://port-0-infli-chat-server-12fhqa2bln5ichzo.sel5.cloudtype.app/");
 
 function App() {
   return (
